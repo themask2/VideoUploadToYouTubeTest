f2e91f3add3733e2d90b65a1ea92566c5d419ff4
ds.barbosa <ds.barbosa@dmz.org.br>
2020-09-01 15:30:26 -0300

    YouTube Test - Uploading a video

---
+++ Samples/WebAuthenticationBroker/cs/Scenario4_Google.xaml.cs
@@ -0,0 +1,221 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Plus.v1;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using SDKTemplate;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Web.Http;
+
+namespace WebAuthentication
+{
+    public sealed partial class Scenario4_Google : Page
+    {
+
+        MainPage rootPage = MainPage.Current;
+        private FileOpenPicker fileOpenPicker;
+        private StorageFile pickedVideoFile;
+        private string filePath;
+        private List<StorageFile> pickedVideoFiles;
+
+
+        String ClientID = "372426487674-ruspbkhcu5rvlavofl78ib4vfbuvl0nr.apps.googleusercontent.com";
+        String ClientSecret = "ML5Iw5aP2uNTGZuouM9Sb06_";
+        String Response;
+        String Token_Access;
+        String Token_Reresh;
+
+        public Scenario4_Google()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OutputToken(String TokenUri)
+        {
+            GoogleReturnedToken.Text = TokenUri;
+        }
+
+        private async void Launch_Click(object sender, RoutedEventArgs e)
+        {
+            if (GoogleCallbackUrl.Text == "")
+            {
+                rootPage.NotifyUser("Please enter an Callback URL.", NotifyType.StatusMessage);
+            }
+
+            try
+            {
+                String GoogleURL = "https://accounts.google.com/o/oauth2/auth?client_id=" + ClientID + "&redirect_uri=" + Uri.EscapeDataString(GoogleCallbackUrl.Text) + "&response_type=code&scope=" + Uri.EscapeDataString("https://www.googleapis.com/auth/youtube.upload") + "+" + Uri.EscapeDataString("https://www.googleapis.com/auth/youtube") + "+" + Uri.EscapeDataString("https://www.googleapis.com/auth/youtubepartner");
+
+                Uri StartUri = new Uri(GoogleURL);
+                // When using the desktop flow, the success code is displayed in the html title of this end uri
+                Uri EndUri = new Uri("https://accounts.google.com/o/oauth2/approval");
+
+                rootPage.NotifyUser("Navigating to: " + GoogleURL, NotifyType.StatusMessage);
+
+                WebAuthenticationResult WebAuthenticationResult = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.UseTitle, StartUri, EndUri);
+                if (WebAuthenticationResult.ResponseStatus == WebAuthenticationStatus.Success)
+                {
+                    OutputToken(WebAuthenticationResult.ResponseData.ToString());
+                    var autorizationCode = WebAuthenticationResult.ResponseData.Substring(13);
+                    var pairs = new Dictionary<string, string>();
+                    pairs.Add("code", autorizationCode);
+                    pairs.Add("client_id", ClientID);
+                    pairs.Add("client_secret", ClientSecret);
+                    pairs.Add("redirect_uri", "urn:ietf:wg:oauth:2.0:oob");
+                    pairs.Add("grant_type", "authorization_code");
+
+                    var formContent = new HttpFormUrlEncodedContent(pairs);
+
+                    var client = new Windows.Web.Http.HttpClient();
+                    var httpResponseMessage = await client.PostAsync(new Uri("https://accounts.google.com/o/oauth2/token"), formContent);
+                    Response = await httpResponseMessage.Content.ReadAsStringAsync();
+                }
+                else if (WebAuthenticationResult.ResponseStatus == WebAuthenticationStatus.ErrorHttp)
+                {
+                    OutputToken("HTTP Error returned by AuthenticateAsync() : " + WebAuthenticationResult.ResponseErrorDetail.ToString());
+                }
+                else
+                {
+                    OutputToken("Error returned by AuthenticateAsync() : " + WebAuthenticationResult.ResponseStatus.ToString());
+                }
+                int pFrom = Response.IndexOf("\"access_token\": ") + "\"access_token\": ".Length;
+                int pTo = Response.LastIndexOf("\"expires_in\":");
+
+                this.Token_Access = Response.Substring(pFrom, pTo - pFrom).Replace("\"", "").Replace(",", "").Replace("\n","").Trim();
+
+                pFrom = Response.IndexOf("\"refresh_token\": ") + "\"refresh_token\": ".Length;
+                pTo = Response.LastIndexOf("\"scope\"");
+                this.Token_Reresh = Response.Substring(pFrom, pTo - pFrom).Replace("\"", "").Replace(",", "").Replace("\n", "").Trim();
+            }
+            catch (Exception Error)
+            {
+                rootPage.NotifyUser(Error.Message, NotifyType.ErrorMessage);
+            }
+        }
+
+        private async void SubirVideoAsync(object sender, RoutedEventArgs e)
+        {
+            fileOpenPicker = GetFileOpenPicker(new List<string> { ".avi", ".mp4", ".wmv" }, PickerLocationId.VideosLibrary);
+            this.pickedVideoFile = await fileOpenPicker.PickSingleFileAsync();
+            this.pickedVideoFiles = new List<StorageFile> { pickedVideoFile };
+            this.filePath = pickedVideoFiles[0].Path;
+
+            try
+            {
+                await Task.Run(UploadTheFile);
+            }
+            catch (AggregateException ex)
+            {
+                Debug.Write("Error: " + ex.Message);
+            }
+        }
+
+        public static FileOpenPicker GetFileOpenPicker(List<string> fileTypes, PickerLocationId locationId = PickerLocationId.DocumentsLibrary, PickerViewMode viewMode = PickerViewMode.Thumbnail)
+        {
+            FileOpenPicker fileOpenPicker = new FileOpenPicker
+            {
+                SuggestedStartLocation = locationId,
+                ViewMode = viewMode
+            };
+            fileTypes.ForEach((fileType) => fileOpenPicker.FileTypeFilter.Add(fileType));
+            return fileOpenPicker;
+        }
+
+        private async Task UploadTheFile()
+        {
+//            string[] scopes = new string[] {
+//                PlusService.Scope.PlusLogin,
+//                PlusService.Scope.UserinfoEmail,
+//                PlusService.Scope.UserinfoProfile
+//};
+
+//            var flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+//            {
+//                ClientSecrets = new ClientSecrets
+//                {
+//                    ClientId = this.ClientID,
+//                    ClientSecret = this.ClientSecret
+//                },
+//                Scopes = scopes,
+//                DataStore = new FileDataStore("Store")
+//            });
+
+//            var token = new TokenResponse
+//            {
+//                AccessToken = Token_Access,
+//                RefreshToken = Token_Reresh
+//            };
+
+//            var credential = new UserCredential(flow, Environment.UserName, token);
+
+            GoogleCredential cred = GoogleCredential.FromAccessToken(Token_Access);
+
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = cred,
+                ApplicationName = "MyYouTubeTest"
+            });
+
+
+            //String CLIENT_ID = "648437414732-vhfugp8lu8q45qvupr9dfbbri27b6t2s.apps.googleusercontent.com";
+            //String CLIENT_SECRET = "whgv3IalKr3BCaL1URwkb0YE";
+            //var youtubeService = AuthenticateOauth(CLIENT_ID, CLIENT_SECRET, "user");
+
+            var video = new Video();
+            video.Snippet = new VideoSnippet();
+            video.Snippet.Title = "Default Video Title";
+            video.Snippet.Description = "Default Video Description";
+            video.Snippet.Tags = new string[] { "tag1", "tag2" };
+            video.Snippet.CategoryId = "22"; // See https://developers.google.com/youtube/v3/docs/videoCategories/list
+            video.Status = new VideoStatus();
+            video.Status.PrivacyStatus = "unlisted"; // or "private" or "public"
+            //var filePath = this.filePath; // Replace with path to actual movie file.
+
+
+
+
+
+            using (var fileStream = (await pickedVideoFiles[0].OpenAsync(Windows.Storage.FileAccessMode.Read)).AsStream())
+            {
+                var videosInsertRequest = youtubeService.Videos.Insert(video, "snippet,status", fileStream, "video/*");
+                videosInsertRequest.ProgressChanged += videosInsertRequest_ProgressChanged;
+                videosInsertRequest.ResponseReceived += videosInsertRequest_ResponseReceived;
+
+                await videosInsertRequest.UploadAsync();
+            }
+        }
+
+        void videosInsertRequest_ProgressChanged(IUploadProgress progress)
+        {
+            switch (progress.Status)
+            {
+                case UploadStatus.Uploading:
+                    Debug.Write("{0} bytes sent.", progress.BytesSent.ToString());
+                    break;
+
+                case UploadStatus.Failed:
+                    Debug.Write("An error prevented the upload from completing.\n{0}", progress.Exception.ToString());
+                    break;
+            }
+        }
+
+        void videosInsertRequest_ResponseReceived(Video video)
+        {
+            Debug.Write("Video id '{0}' was successfully uploaded.", video.Id);
+        }
+    }
+}
